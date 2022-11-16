ff354ede67f499143bab3efe7418fe442873e8c9
golfang01 <117799607+golfang01@users.noreply.github.com>
2022-11-16 13:54:07 +0700

    Add files via upload

---
+++ testhome.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="style-home.css" rel="stylesheet">
+    <link href="card-style.css" rel="stylesheet">
+</head>
+<body>
+    <div id="toggle"></div>
+    <div id="sidebar">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="logout.inc.php">Log out</a></li>
+        </ul>
+    </div>
+    <div class="container">
+  <div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>01</h2>
+        <h3>ปวช.1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+        <a href="#">Read More</a>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>02</h2>
+        <h3>ปวช.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>03</h2>
+        <h3>ปวช.3</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
+  </div>
+
+
+<div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>04</h2>
+        <h3>ปวส.1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
+  </div>
+
+
+<div class="card">
+    <div class="box">
+      <div class="content">
+        <h2>05</h2>
+        <h3>ปวส.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
+  </div>
+  </div>
+
+<script>
+    const toggle = document.getElementById('toggle');
+    const sidebar = document.getElementById('sidebar');
+
+    document.onclick = function(e){
+        if(e.target.id !== 'sidebar' && e.target.id !== 'toggle')
+        {
+            toggle.classList.remove('active');
+            sidebar.classList.remove('active');
+        }
+    }
+
+    
+    
+    toggle.onclick = function(){
+        toggle.classList.toggle('active');
+        sidebar.classList.toggle('active');
+    }
+    </script>
+</body>
+</html>
