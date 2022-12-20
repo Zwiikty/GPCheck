f1c33398980b924651a6b7f6817ff4973137e6ea
Zwiikty <67731522+Zwiikty@users.noreply.github.com>
2022-12-20 22:40:11 +0700

    sql 20/12/22

--- gpcheckdb.sql
+++ gpcheckdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2022 at 04:22 PM
+-- Generation Time: Dec 20, 2022 at 04:39 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -42,9 +42,9 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`r_id`, `r_code`, `r_name`, `r_time`, `r_latitude`, `r_longitude`, `r_teacher`) VALUES
-(31, 'imxot3', 'admin', '2022-12-12 20:26:00', '', '', 'Admin '),
-(34, 'zObr5Z', '14/12/2022', '2022-12-14 10:21:00', '', '', 'Teacher1'),
-(35, 'kSfeVs', '19/12/22', '2022-12-19 14:18:00', '', '', 'Teacher1');
+(34, 'zObr5Z', '55555555555', '2022-12-21 10:21:00', '', '', 'Teacher1'),
+(35, 'kSfeVs', '19/12/22', '2022-12-19 14:18:00', '', '', 'Teacher1'),
+(36, 'q7fu9o', '20/12/2022', '2022-12-20 21:26:00', '', '', 'Teacher1');
 
 -- --------------------------------------------------------
 
@@ -71,11 +71,11 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `user_username`, `user_password`, `user_name`, `user_status`, `user_r_code`, `user_latitude`, `user_longitude`, `user_time`, `user_checkstatus`) VALUES
 (1, 'admin', 'admin123', 'Admin ', 0, '', 0, 0, '2022-11-21 00:00:00', 0),
-(2, '64309010030', '13/09/2545', 'Pisit Audjai', 1, 'kSfeVs', 0, 0, '2022-12-19 14:30:20', 1),
+(2, '64309010030', '13/09/2545', 'Pisit Audjai', 1, 'q7fu9o', 0, 0, '2022-12-20 21:29:41', 1),
 (4, 'teacher', 'teacher123', 'Teacher1', 2, '', 0, 0, '2022-12-08 07:21:39', 0),
 (13, 'student1', 'student123', 'student', 1, '', 0, 0, '0000-00-00 00:00:00', 0),
 (14, 'teacher1', 'teacher123', 'teacher2', 2, '', 0, 0, '0000-00-00 00:00:00', 0),
-(15, '12345', '12345', '12345', 1, '', 0, 0, '0000-00-00 00:00:00', 0);
+(16, 'test66', 'test666', 'test6666', 2, '', 0, 0, '0000-00-00 00:00:00', 0);
 
 --
 -- Indexes for dumped tables
@@ -101,13 +101,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
