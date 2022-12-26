a3712785f925d314c5be8bed23d929560f169a3f
Zwiikty <67731522+Zwiikty@users.noreply.github.com>
2022-12-26 20:37:18 +0700

    sql 26/12/22

--- gpcheckdb.sql
+++ gpcheckdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2022 at 04:39 PM
+-- Generation Time: Dec 26, 2022 at 02:36 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -32,8 +32,8 @@ CREATE TABLE `room` (
   `r_code` varchar(6) NOT NULL,
   `r_name` varchar(100) NOT NULL,
   `r_time` datetime NOT NULL,
-  `r_latitude` varchar(10) NOT NULL,
-  `r_longitude` varchar(10) NOT NULL,
+  `r_latitude` double NOT NULL,
+  `r_longitude` double NOT NULL,
   `r_teacher` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,9 +42,11 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`r_id`, `r_code`, `r_name`, `r_time`, `r_latitude`, `r_longitude`, `r_teacher`) VALUES
-(34, 'zObr5Z', '55555555555', '2022-12-21 10:21:00', '', '', 'Teacher1'),
-(35, 'kSfeVs', '19/12/22', '2022-12-19 14:18:00', '', '', 'Teacher1'),
-(36, 'q7fu9o', '20/12/2022', '2022-12-20 21:26:00', '', '', 'Teacher1');
+(39, 'bWXPBt', 'tester', '2022-12-24 20:05:00', 18.289168, 99.483426, 'Teacher1'),
+(42, '0mBeAo', 'NY', '2022-12-26 19:52:00', 40.71285, -74.013991, 'Teacher1'),
+(43, '4SQWvV', 'LA', '2022-12-26 19:52:00', 34.008369, -118.498669, 'Teacher1'),
+(44, 'pBLCun', 'Hali Iceland', '2022-12-26 19:54:00', 64.129811, -16.017196, 'Teacher1'),
+(45, 'VnmObx', 'ข้างบ้าน', '2022-12-26 19:54:00', 18.249292, 99.488585, 'Teacher1');
 
 -- --------------------------------------------------------
 
@@ -59,8 +61,8 @@ CREATE TABLE `user` (
   `user_name` varchar(25) NOT NULL,
   `user_status` int(1) NOT NULL,
   `user_r_code` varchar(6) NOT NULL,
-  `user_latitude` int(10) NOT NULL,
-  `user_longitude` int(10) NOT NULL,
+  `user_latitude` double NOT NULL,
+  `user_longitude` double NOT NULL,
   `user_time` datetime NOT NULL,
   `user_checkstatus` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -71,9 +73,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `user_username`, `user_password`, `user_name`, `user_status`, `user_r_code`, `user_latitude`, `user_longitude`, `user_time`, `user_checkstatus`) VALUES
 (1, 'admin', 'admin123', 'Admin ', 0, '', 0, 0, '2022-11-21 00:00:00', 0),
-(2, '64309010030', '13/09/2545', 'Pisit Audjai', 1, 'q7fu9o', 0, 0, '2022-12-20 21:29:41', 1),
+(2, '64309010030', '13/09/2545', 'Pisit Audjai', 1, 'bWXPBt', 18.8160541, 98.9848536, '2022-12-26 19:10:01', 1),
 (4, 'teacher', 'teacher123', 'Teacher1', 2, '', 0, 0, '2022-12-08 07:21:39', 0),
-(13, 'student1', 'student123', 'student', 1, '', 0, 0, '0000-00-00 00:00:00', 0),
+(13, 'student1', 'student123', 'student', 1, 'bWXPBt', 18.81605339050293, 98.98485565185547, '2022-12-26 18:32:48', 1),
 (14, 'teacher1', 'teacher123', 'teacher2', 2, '', 0, 0, '0000-00-00 00:00:00', 0),
 (16, 'test66', 'test666', 'test6666', 2, '', 0, 0, '0000-00-00 00:00:00', 0);
 
@@ -101,7 +103,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `user`
